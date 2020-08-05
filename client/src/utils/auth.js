e973358c8f2ac33405c73c898e85243592b40c1f
Diab <emailcodydiab@gmail.com>
2020-08-05 09:45:48 -0500

    add token to all requests

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -16,7 +16,7 @@ loggedIn() {
 // check if token has expired
 isTokenExpired(token){
     try{
-        const decoded = decoded(token);
+        const decoded = decode(token);
         if (decoded.exp < Date.now()/1000) {
             return true;
         }else {
